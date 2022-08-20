10ce26a038264ac84fd5feff121f5322a05efc7d
Takayuki Hira <pettanco3@gmail.com>
2022-08-20 14:45:02 +0900

    change mapping

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index() {
+    @RequestMapping("/hello")
+    public String hello() {
         return "hello";
     }
 }
