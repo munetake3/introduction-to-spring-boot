d8aad3eeffb74075b550c3333f814336b19aff49
Takayuki Hira <pettanco3@gmail.com>
2022-08-22 01:06:09 +0900

    fix Model annotation Data -> Getter and Setter

--- spring-mvc1/src/main/java/com/example/demo/model/User.java
+++ spring-mvc1/src/main/java/com/example/demo/model/User.java
@@ -1,8 +1,10 @@
 package com.example.demo.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class User {
     private String name;
     private String email;
